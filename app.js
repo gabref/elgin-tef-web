e0e5372f0d535948ac29e712211b08464c30486a
Gabriel Franzeri <gabrielfranzer@gmail.com>
2023-04-25 13:19:33 -0300

    feat: buttons funciontality

---
+++ app.js
@@ -0,0 +1,101 @@
+const BASE_URL = 'http://localhost:2001/tef/v1'
+
+function start() {
+    window.location.href = "intent://connect/start"
+}
+
+function stop() {
+    window.location.href = "intent://connect/stop"
+}
+
+function configuracao() {
+    sendPost('/configuracao', {
+        nome: "nome teste",
+        versao: "0.0.1",
+        textoPinpad: "teste texto",
+        macPinpad: "",
+        producao: "0",
+        estabelecimento: "estabelecimento teste",
+        terminal: "000000",
+        loja: ""
+    })
+}
+
+function ativacao() {
+    sendPost('/ativacao', {
+        cnpjCpf: "14.200.166/0001-66"
+    })
+}
+
+function reimpressao() {
+    sendGet('/adm/reimpressao')
+}
+
+function relatorio() {
+    sendGet('/adm/relatorio')
+}
+
+function venda() {
+    sendPost('/venda', null)
+}
+
+function debito() {
+    sendPost('/venda/debito', {
+        valor: "1.00"
+    })
+}
+
+function credito() {
+    sendPost('/venda/credito', {
+        valor: "1.00",
+        parcelas: "1",
+        financiamento: "1"
+    })
+}
+
+function cancelamento() {
+    sendPost('/adm/cancelamento', {
+        valor: "1.00",
+        nsu: "000000",
+        data: "01/09/22"
+    })
+}
+
+async function sendGet(rota) {
+    try {
+        const responseData = await fetch(BASE_URL + rota, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "GET"
+        })
+        const data = await responseData.text()
+
+        console.log(data)
+        alert(data)
+    } catch (err) {
+        console.error(err)
+        alert(err)
+    }
+}
+
+async function sendPost(rota, body) {
+    try {
+        const responseData = await fetch(BASE_URL + rota, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(body)
+        })
+        const data = await responseData.text()
+        
+        console.log('success: ', data)
+        alert(data)
+    } catch (err) {
+        console.error('error: ', err)
+        alert(err)
+    }
+}
